03320a5d880a1d964a023cdb0e5ba7946578e8c0
Liquan (Max) Gu <maxlg@chromium.org>
2019-12-17 13:51:45 -0500

    change promistresolver

--- pr/apps/max-openwindow-google/app.js
+++ pr/apps/max-openwindow-google/app.js
@@ -1,20 +1,11 @@
 const urlDifferentOrigin = "https://www.google.com";
 
 self.addEventListener('paymentrequest', e => {
-  resolver = new PromiseResolver();
-  e.respondWith(resolver.promise);
-  e.openWindow(urlDifferentOrigin).then(client => {
-    if (client === null) {
-      resolver.reject('Failed to open window');
+  evt.respondWith(new Promise((resolve) => {
+    const promise = evt.openWindow(urlDifferentOrigin);
+    if (!promise) {
+      console.log("failed to openWindow");
+      resolve("Failed to openWindow");
     }
-  }).catch(err => {
-    resolver.reject(err);
-  });
-  // evt.respondWith(new Promise((resolve) => {
-  //   const promise = evt.openWindow('https://www.google.com');
-  //   if (!promise) {
-  //     console.log("failed to openWindow");
-  //     resolve("Failed to openWindow");
-  //   }
-  // }));
+  }));
 });
--- pr/apps/max-openwindow-nonexist/app.js
+++ pr/apps/max-openwindow-nonexist/app.js
@@ -1,13 +1,11 @@
-const urlDifferentOrigin = "https://jlkasdflklksdl";
+const urlDifferentOrigin = "https://asdfasdfasdfs";
 
 self.addEventListener('paymentrequest', e => {
-  resolver = new PromiseResolver();
-  e.respondWith(resolver.promise);
-  e.openWindow(urlDifferentOrigin).then(client => {
-    if (client === null) {
-      resolver.reject('Failed to open window');
+  evt.respondWith(new Promise((resolve) => {
+    const promise = evt.openWindow(urlDifferentOrigin);
+    if (!promise) {
+      console.log("failed to openWindow");
+      resolve("Failed to openWindow");
     }
-  }).catch(err => {
-    resolver.reject(err);
-  });
+  }));
 });
