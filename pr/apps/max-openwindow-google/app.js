f9bb65b042c12ed4c6ead1ea3f1f7ab1b5398eca
Liquan (Max) Gu <maxlg@chromium.org>
2019-12-17 12:23:41 -0500

    fix app

--- pr/apps/max-openwindow-google/app.js
+++ pr/apps/max-openwindow-google/app.js
@@ -1,5 +1,20 @@
-self.addEventListener('paymentrequest', (evt) => {
-  evt.respondWith(new Promise((resolve) => {
-    evt.openWindow('https://www.google.com');
-  }));
+const urlDifferentOrigin = "https://www.google.com";
+
+self.addEventListener('paymentrequest', e => {
+  resolver = new PromiseResolver();
+  e.respondWith(resolver.promise);
+  e.openWindow(urlDifferentOrigin).then(client => {
+    if (client === null) {
+      resolver.reject('Failed to open window');
+    }
+  }).catch(err => {
+    resolver.reject(err);
+  });
+  // evt.respondWith(new Promise((resolve) => {
+  //   const promise = evt.openWindow('https://www.google.com');
+  //   if (!promise) {
+  //     console.log("failed to openWindow");
+  //     resolve("Failed to openWindow");
+  //   }
+  // }));
 });
