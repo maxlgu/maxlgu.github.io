0a9f10b1fb0838221e9fd22f13bcc59d25b3c28f
Liquan (Max) Gu <maxlg@chromium.org>
2020-01-03 14:16:17 -0500

    browsertest

---
+++ pr/browsertest/merchant.js
@@ -0,0 +1,15 @@
+/**
+ * Launches the payment handler.
+ */
+async function launch() { // eslint-disable-line no-unused-vars
+  try {
+    const request = new PaymentRequest([{supportedMethods: methodName}], {
+      total: {label: 'Total', amount: {currency: 'USD', value: '0.01'}},
+    });
+    const response = await request.show();
+    await response.complete('success');
+    return 'success';
+  } catch (e) {
+    return e.toString();
+  }
+}
