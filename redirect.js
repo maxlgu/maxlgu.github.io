2ad542a3147cc3aafbf8e241316498b03fff318d
Liquan (Max) Gu <maxlg@chromium.org>
2021-04-07 18:13:29 -0400

    save

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-if ((window.location.host === 'rsolomakhin.github.io') &&
+if ((window.location.host === 'maxlgu.github.io') &&
     (window.location.protocol !== 'https:')) {
   window.location.protocol = 'https:';
 }
