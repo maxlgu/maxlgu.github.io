185dfe98184b8cb4545bc580a05baff9e25a9688
Liquan (Max) Gu <maxlg@chromium.org>
2021-07-15 13:34:40 -0400

    update test page

--- pr/spc/pr.js
+++ pr/spc/pr.js
@@ -1,55 +1,180 @@
 /* exported createPaymentCredential */
 /* exported onBuyClicked */
-
 const textEncoder = new TextEncoder();
-
+/**
+ * Converts an ArrayBuffer into a string.
+ */
+function arrayBufferToString(input) {
+  return String.fromCharCode(...new Uint8Array(input));
+}
+/**
+ * Converts an ArrayBuffer into a base64 encoded string.
+ */
+function arrayBufferToBase64(input) {
+  return btoa(arrayBufferToString(input));
+}
+/**
+ * Converts a base64 encoded string into Unit8Array.
+ */
+function base64ToArray(input) {
+  return Uint8Array.from(atob(input), c => c.charCodeAt(0))
+}
+/**
+ * Converts a PaymentResponse or a PublicKeyCredential into an dictionary.
+ */
+function objectToDictionary(input) {
+  let output = {};
+  if (input.requestId) {
+    output.requestId = input.requestId;
+  }
+  if (input.id) {
+    output.id = input.id;
+  }
+  if (input.rawId && input.rawId.constructor === ArrayBuffer) {
+    output.rawId = arrayBufferToBase64(input.rawId);
+  }
+  if (input.response && (input.response.constructor ===
+      AuthenticatorAttestationResponse || input.response.constructor ===
+      AuthenticatorAssertionResponse || input.response.constructor === Object
+      )) {
+    output.response = objectToDictionary(input.response);
+  }
+  if (input.attestationObject && input.attestationObject.constructor ===
+    ArrayBuffer) {
+    output.attestationObject = arrayBufferToBase64(input.attestationObject);
+  }
+  if (input.authenticatorData && input.authenticatorData.constructor ===
+    ArrayBuffer) {
+    output.authenticatorData = arrayBufferToBase64(input.authenticatorData);
+  }
+  if (input.authenticatorData && input.authenticatorData.constructor ===
+    String) {
+    output.authenticatorData = input.authenticatorData;
+  }
+  if (input.clientDataJSON && input.clientDataJSON.constructor ===
+    ArrayBuffer) {
+    output.clientDataJSON = arrayBufferToString(input.clientDataJSON);
+  }
+  if (input.clientDataJSON && input.clientDataJSON.constructor ===
+    String) {
+    output.clientDataJSON = atob(input.clientDataJSON);
+  }
+  if (input.info) {
+    output.info = objectToDictionary(input.info);
+  }
+  if (input.signature && input.signature.constructor === ArrayBuffer) {
+    output.signature = arrayBufferToBase64(input.signature);
+  }
+  if (input.signature && input.signature.constructor === String) {
+    output.signature = input.signature;
+  }
+  if (input.userHandle && input.userHandle.constructor === ArrayBuffer) {
+    output.userHandle = arrayBufferToBase64(input.userHandle);
+  }
+  if (input.userHandle && input.userHandle.constructor === String) {
+    output.userHandle = input.userHandle;
+  }
+  if (input.type) {
+    output.type = input.type;
+  }
+  if (input.methodName) {
+    output.methodName = input.methodName;
+  }
+  if (input.details) {
+    output.details = objectToDictionary(input.details);
+  }
+  if (input.appid_extension) {
+    output.appid_extension = input.appid_extension;
+  }
+  if (input.challenge) {
+    output.challenge = input.challenge;
+  }
+  if (input.echo_appid_extension) {
+    output.echo_appid_extension = input.echo_appid_extension;
+  }
+  if (input.echo_prf) {
+    output.echo_prf = input.echo_prf;
+  }
+  if (input.prf_not_evaluated) {
+    output.prf_not_evaluated = input.prf_not_evaluated;
+  }
+  if (input.prf_results) {
+    output.prf_results = objectToDictionary(input.prf_results);
+  }
+  if (input.user_handle) {
+    output.user_handle = input.user_handle;
+  }
+  if (input.authenticator_data) {
+    output.authenticator_data = input.authenticator_data;
+  }
+  if (input.client_data_json) {
+    output.client_data_json = atob(input.client_data_json);
+  }
+  if (input.shippingAddress) {
+    output.shippingAddress = input.shippingAddress;
+  }
+  if (input.shippingOption) {
+    output.shippingOption = input.shippingOption;
+  }
+  if (input.payerName) {
+    output.payerName = input.payerName;
+  }
+  if (input.payerEmail) {
+    output.payerEmail = input.payerEmail;
+  }
+  if (input.payerPhone) {
+    output.payerPhone = input.payerPhone;
+  }
+  return output;
+}
+/**
+ * Converts a PaymentResponse or a PublicKeyCredential into a string.
+ */
+function objectToString(input) {
+  return JSON.stringify(objectToDictionary(input), undefined, 2);
+}
 /**
  * Creates a payment credential.
  */
 async function createPaymentCredential(windowLocalStorageIdentifier) {
   const rp = {
-    id: 'maxlgu.github.io',
-    name: 'Max',
+    id: window.location.hostname,
+    name: 'May Pay',
+  };
+  const pubKeyCredParams = [{
+    type: 'public-key',
+    alg: -7, // ECDSA, not supported on Windows.
+  }, {
+    type: 'public-key',
+    alg: -257, // RSA, supported on Windows.
+  }];
+  const authenticatorSelection = {
+    userVerification: 'required',
   };
   const instrument = {
     displayName: 'Max Pay',
     icon: 'https://maxlgu.github.io/pr/spc/troy.png',
   };
-  const pubKeyCredParams = [{
-    type: 'public-key',
-    alg: -7,  // ECDSA, not supported on Windows.
-  }, {
-    type: 'public-key',
-    alg: -257,  // RSA, supported on Windows.
-  }];
-  const authenticatorSelection = {
-    authenticatorAttachment: 'platform',
-    requireResidentKey: false,
-    userVerification: 'discouraged',
-  };
   const payment = {
     rp,
     instrument,
-    challenge: textEncoder.encode('Transaction approval challenge'),
+    challenge: textEncoder.encode('Enrollment challenge'),
     pubKeyCredParams,
     authenticatorSelection,
-    attestation: "none",
   };
   try {
-    const publicKeyCredential = await navigator.credentials.create({payment});
-    info("created publicKeyCredential: "+ publicKeyCredential);
-    window.localStorage.setItem(
-        windowLocalStorageIdentifier,
-        btoa(String.fromCharCode(...new Uint8Array(
-            publicKeyCredential.rawId))));
-    info(windowLocalStorageIdentifier + ': Credential ' +
-         window.localStorage.getItem(windowLocalStorageIdentifier) +
-         ' enrolled.');
+    const publicKeyCredential = await navigator.credentials.create({
+      payment
+    });
+    console.log(publicKeyCredential);
+    window.localStorage.setItem(windowLocalStorageIdentifier,
+      arrayBufferToBase64(publicKeyCredential.rawId));
+    info(windowLocalStorageIdentifier + ' enrolled: ' + objectToString(
+      publicKeyCredential));
   } catch (err) {
     error(err);
   }
 }
-
 /**
  * Initializes the payment request object.
  * @return {PaymentRequest} The payment request object.
@@ -58,25 +183,28 @@ async function buildPaymentRequest(windowLocalStorageIdentifier) {
   if (!window.PaymentRequest) {
     return null;
   }
-
   let request = null;
-
   try {
     // Documentation:
     // https://github.com/rsolomakhin/secure-payment-confirmation
+    const challenge = textEncoder.encode('network_data');
+    const updatedInstrument = {
+      displayName: 'Max Pay',
+      icon: 'https://maxlgu.github.io/pr/spc/troy-alt-logo.png',
+    };
     const supportedInstruments = [{
       supportedMethods: 'secure-payment-confirmation',
       data: {
         action: 'authenticate',
-        credentialIds: [Uint8Array.from(
-            atob(window.localStorage.getItem(windowLocalStorageIdentifier)),
-            c => c.charCodeAt(0))],
-        networkData: textEncoder.encode('network_data'),
+        credentialIds: [base64ToArray(window.localStorage.getItem(
+          windowLocalStorageIdentifier))],
+        instrument: updatedInstrument,
+        networkData: challenge,
+        challenge,
         timeout: 60000,
         fallbackUrl: 'https://maxlgu.github.io/pr/spc/fallback'
       },
     }];
-
     const details = {
       total: {
         label: 'Total',
@@ -86,15 +214,12 @@ async function buildPaymentRequest(windowLocalStorageIdentifier) {
         },
       },
     };
-
     request = new PaymentRequest(supportedInstruments, details);
   } catch (err) {
     error(err);
   }
-
   return request;
 }
-
 /**
  * Launches payment request for Android Pay.
  */
@@ -103,20 +228,18 @@ async function onBuyClicked(windowLocalStorageIdentifier) {
     error('PaymentRequest API is not supported.');
     return;
   }
-
   const request = await buildPaymentRequest(windowLocalStorageIdentifier);
-  if (!request)
-    return;
-
+  if (!request) return;
   try {
     const instrumentResponse = await request.show();
     await instrumentResponse.complete('success')
-    info(windowLocalStorageIdentifier + ': your payment is successful!\n\n' + JSON.stringify(instrumentResponse, undefined, 2));
+    console.log(instrumentResponse);
+    info(windowLocalStorageIdentifier + ' payment response: ' +
+      objectToString(instrumentResponse));
   } catch (err) {
     error(err);
   }
 }
-
 async function checkCanMakePayment(windowLocalStorageIdentifier) {
   if (!window.PaymentRequest) {
     error('PaymentRequest API is not supported.');
@@ -124,36 +247,32 @@ async function checkCanMakePayment(windowLocalStorageIdentifier) {
   }
   try {
     const request = await buildPaymentRequest(windowLocalStorageIdentifier);
-    if (!request)
-      return;
+    if (!request) return;
     const result = await request.canMakePayment();
-    info(windowLocalStorageIdentifier + ': ' + (result ? 'Can make payment.' : 'Cannot make payment'));
+    info((result ? 'Can make payment.' : 'Cannot make payment'));
   } catch (err) {
     error(err);
   }
 }
-
-/* Get the documentElement (<html>) to display the page in fullscreen */
-var elem = document.documentElement;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-/* Close fullscreen */
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
-    document.msExitFullscreen();
+async function webAuthnGet(windowLocalStorageIdentifier) {
+  try {
+    const publicKey = {
+      challenge: textEncoder.encode('Authentication challenge'),
+      userVerification: 'required',
+      allowCredentials: [{
+        transports: ['internal'],
+        type: 'public-key',
+        id: base64ToArray(window.localStorage.getItem(
+          windowLocalStorageIdentifier)),
+      }, ],
+    };
+    const credentialInfoAssertion = await navigator.credentials.get({
+      publicKey
+    });
+    console.log(credentialInfoAssertion);
+    info('Successful login with ' + windowLocalStorageIdentifier + ': ' +
+      objectToString(credentialInfoAssertion));
+  } catch (err) {
+    error(err);
   }
 }
