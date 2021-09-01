e179dfbc17b000303108e45fc7070f8db2429a6d
Liquan (Max) Gu <maxlg@chromium.org>
2021-09-01 15:55:24 -0400

    isUVPAA

--- pr/webauthn/pr.js
+++ pr/webauthn/pr.js
@@ -3,3 +3,8 @@ async function isUVPAA() {
     const result = await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable();
     info('isUserVerifyingPlatformAuthenticatorAvailable=' + result);
 }
+
+async function isUVPAAs() {
+  await isUVPAA();
+  await isUVPAA();
+}
