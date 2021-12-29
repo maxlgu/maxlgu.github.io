000f8b25bc1d256b4e97d8a0559fbc50d09c9afb
Liquan (Max) Gu <maxlg@chromium.org>
2021-12-29 13:50:41 -0500

    popup

--- pr/util.js
+++ pr/util.js
@@ -148,6 +148,15 @@ function changeWindowIn10s() {
   }, 10000);
 }
 
+function popup(mylink, windowname) {
+  if (! window.focus)return true;
+  var href;
+  if (typeof(mylink) == 'string') href=mylink;
+  else href=mylink.href;
+  window.open(href, windowname, 'width=400,height=200,scrollbars=yes');
+  return false;
+}
+
 let reportingOptions = {
   types: ['deprecation'],
   buffered: true
